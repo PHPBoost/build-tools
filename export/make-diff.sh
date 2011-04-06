2fc520a35b6f45dcae728fa204f2eba285b347dc
Benoit Sautel <Benoit Sautel@localhost>
2011-04-06 22:45:34 +0200

    [Fix] Adaptation du script construisant les patches de mise ? jour pour Mercurial

--- export/make-diff.sh
+++ export/make-diff.sh
@@ -1,23 +1,36 @@
 #!/bin/bash
 
-old=$1
-new=$2
-cd $3
-destination=`pwd`
-repository=http://phpboost.googlecode.com/svn/tags
+cd $(dirname $0) && here=$(pwd) && cd - > /dev/null
+binDir=$here/../../bin/
+repository=$1
+old=$2
+new=$3
 tmpDir=/tmp/phpboost-diff
 diffFile=diff-$old-to-$new.zip
 diffFileOptimized=diff-$old-to-$new-optimized.zip
 
-cd $SVN_PATH/tags/
-svn update $new && cd $new
+cd $4
+destination=`pwd`
+cd - > /dev/null
+
 rm -rf $tmpDir && mkdir $tmpDir
 
 echo 'computing revision history'
-echo "build non-optimized patch to $tmpDir/$diffFile"
 
-for file in `svn diff --old="${repository}/${old}" --new="${repository}/${new}" --summarize | sed -e "s#[A-Z]\s*${repository}/${old}/##"`
+# computing modified files
+cd $1
+raw_diff_output=$(hg diff -r ${old}:${new} --stat)
+lines_count=$(echo -e "$raw_diff_output" | wc -l)
+changed_files=$(echo -e "$raw_diff_output" | head -n $((lines_count - 1)))
+changed_files=$(echo -e "$changed_files" | sed -e "s#\s*|.*\$##")
+echo -e "$changed_files"
+
+hg up -c $new
+
+echo "build non-optimized patch to $tmpDir/$diffFile"
+for file in `echo $changed_files`
 do
+	echo $file
         if [ -f $file ]; then
                 if [[ ! $file =~ ^(install|update)/.+ ]]; then
                         zip $tmpDir/$diffFile $file
@@ -25,13 +38,15 @@ do
         fi
 done
 
+cd - > /dev/null
+
 echo "copy non-optimized patch from  $tmpDir/$diffFile to $destination/$diffFile"
 cp $tmpDir/$diffFile $destination/$diffFile
 
 
 echo "optimize kernel patch"
 cd $tmpDir && unzip $diffFile && rm -f $diffFile && mkdir kernel-optimized
-java -jar $SVN_PATH/tools/POptimizer/poptimizer.jar --ics=iso-8859-1 --ocs=iso-8859-1 -i kernel -o kernel-optimized
+java -jar $binDir/poptimizer.jar --ics=iso-8859-1 --ocs=iso-8859-1 -i kernel -o kernel-optimized
 rm -rf kernel && mv kernel-optimized kernel
 
 echo "build optimized patch to $destination/$diffFileOptimized"
